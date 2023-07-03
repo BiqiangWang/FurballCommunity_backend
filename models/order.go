cb8bd73fb6fa7743ef7249975fa2308a6376b0e2
BiqiangWang <wangbiqiang22@mails.ucas.edu.cn>
2023-07-03 21:41:08 +0800

    更新打卡功能

--- models/order.go
+++ models/order.go
@@ -8,22 +8,24 @@ import (
 )
 
 type Order struct {
-	OrderID      uint       `gorm:"primary_key" json:"order_id"`
-	PetID        uint       `json:"pet_id"`
-	AnnouncerID  uint       `json:"announcer_id"`
-	ReceiverID   uint       `json:"receiver_id"`
-	Pet          Pet        `gorm:"foreign_key:PetID"`
-	OrderCmts    []OrderCmt `gorm:"foreign_key:OrderID"`
-	AnnounceTime time.Time  `json:"announce_time" gorm:"default:CURRENT_TIMESTAMP"`
-	StartTime    time.Time  `json:"start_time" gorm:"default:CURRENT_TIMESTAMP"`
-	EndTime      time.Time  `json:"end_time" gorm:"default:CURRENT_TIMESTAMP"`
-	Place        string     `json:"place"`
-	PetHealth    string     `json:"pet_health"`
-	Status       int        `json:"status"`
-	Remark       string     `json:"remark"`
-	Price        int        `json:"price"`
-	Evaluation   string     `json:"evaluation"`
-	Score        float64    `json:"score"`
+	OrderID         uint       `gorm:"primary_key" json:"order_id"`
+	PetID           uint       `json:"pet_id"`
+	AnnouncerID     uint       `json:"announcer_id"`
+	ReceiverID      uint       `json:"receiver_id"`
+	Pet             Pet        `gorm:"foreign_key:PetID"`
+	OrderCmts       []OrderCmt `gorm:"foreign_key:OrderID"`
+	AnnounceTime    time.Time  `json:"announce_time" gorm:"default:CURRENT_TIMESTAMP"`
+	StartTime       time.Time  `json:"start_time" gorm:"default:CURRENT_TIMESTAMP"`
+	EndTime         time.Time  `json:"end_time" gorm:"default:CURRENT_TIMESTAMP"`
+	Place           string     `json:"place"`
+	PetHealth       string     `json:"pet_health"`
+	Status          int        `json:"status"`
+	Remark          string     `json:"remark"`
+	Price           int        `json:"price"`
+	Evaluation      string     `json:"evaluation"`
+	Score           float64    `json:"score"`
+	CheckInStatus   string     `json:"check_in_status"`
+	CheckInPictures string     `json:"check_in_pictures"`
 }
 
 // BelongsTo 在Order模型中定义BelongsTo方法，表示Order属于一个pet
@@ -78,16 +80,18 @@ func GetOrderInfoByID(orderID uint) (order *Order, err error) {
 
 func UpdateOrderInfo(order *Order) (err error) {
 	err = database.DB.Model(&order).Updates(map[string]interface{}{
-		"receiver_id": order.ReceiverID,
-		"start_time":  order.StartTime,
-		"end_time":    order.EndTime,
-		"place":       order.Place,
-		"pet_health":  order.PetHealth,
-		"status":      order.Status,
-		"remark":      order.Remark,
-		"price":       order.Price,
-		"evaluation":  order.Evaluation,
-		"score":       order.Score,
+		"receiver_id":       order.ReceiverID,
+		"start_time":        order.StartTime,
+		"end_time":          order.EndTime,
+		"place":             order.Place,
+		"pet_health":        order.PetHealth,
+		"status":            order.Status,
+		"remark":            order.Remark,
+		"price":             order.Price,
+		"evaluation":        order.Evaluation,
+		"score":             order.Score,
+		"check_in_status":   order.CheckInStatus,
+		"check_in_pictures": order.CheckInPictures,
 	}).Error
 	return
 }
